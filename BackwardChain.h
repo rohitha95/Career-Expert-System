ed85a1538adeb2d869cde27d07b47b1ade4c39ea
rohitha95 <30647991+rohitha95@users.noreply.github.com>
2019-02-19 13:17:07 -0600

    Update BackwardChain.h

--- BackwardChain.h
+++ BackwardChain.h
@@ -2,6 +2,7 @@
 #define BACKWARDCHAIN_H
 #include<string>
 using namespace std;
+#include<KnowledgeBase.h>
 
 
 class BackwardChain
@@ -9,33 +10,16 @@ class BackwardChain
     public:
         BackwardChain();
         virtual ~BackwardChain();
-        const int CONCLUSION=10;
-        const int VARIABLES=10;
-        const int CLAUSE=40;
-        string conclt[11];
-        /*  variable list */
-        string varlt[11];
-        /*  clause vairable list */
-        string clvarlt[40];
-        string varble;
-        string /* qualify */ qu, /* degree*/ de;
-        string/* discovery */ di, /* position */ po;
-        string buff;
-        int instlt[11];
-        /* statement stack */
-        int statsk[11];
-        int /* clause stack */ clausk[11], sn, f, i, j, s, k, /*stack pointer */ sp;
-
-        float /* grade */ gr, /*experience */ ex;
-
         void determine_member_concl_list(void);
         void push_on_stack(void);
-        void instantiate(void);
-        void inferenceEngine();
-        int KnowledgeBase(int,int);
-        //void process();
-        void process2();
+        void instantiate();
+        string getProfession();
 
+        void inferenceEngine();
+
+        //void process();
+
+        KnowledgeBase kb;
 
 
     protected:
